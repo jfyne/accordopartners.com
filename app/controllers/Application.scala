1c2946e869c973107aee34c6b014a7bb14cb379d
Josh Fyne <josh.fyne@essencedigital.com>
2013-01-27 20:55:39 +0000

    Making it work asynchronously with google spreadsheet

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,9 +7,68 @@ import models._
 
 object Application extends Controller {
 
+    /**
+     * Home
+     *
+     */
     def index = Action {
-        val title = Content.fetch("home.title").get
-        val subtitle = Content.fetch("home.subtitle").get
-        Ok(views.html.index(title, subtitle))
+        Async {
+            val response = Content.getContent
+            response.map({ sheet =>
+                Ok(views.html.index(Content.parse(sheet)))
+            })
+        }
+    }
+
+    /**
+     * About
+     *
+     */
+    def about = Action {
+        Async {
+            val response = Content.getContent
+            response.map({ sheet =>
+                Ok(views.html.about(Content.parse(sheet)))
+            })
+        }
+    }
+
+    /**
+     * Solutions
+     *
+     */
+    def solutions = Action {
+        Async {
+            val response = Content.getContent
+            response.map({ sheet =>
+                Ok(views.html.solutions(Content.parse(sheet)))
+            })
+        }
+    }
+
+    /**
+     * Experience
+     *
+     */
+    def experience = Action {
+        Async {
+            val response = Content.getContent
+            response.map({ sheet =>
+                Ok(views.html.experience(Content.parse(sheet)))
+            })
+        }
+    }
+
+    /**
+     * Contact
+     *
+     */
+    def contact = Action {
+        Async {
+            val response = Content.getContent
+            response.map({ sheet =>
+                Ok(views.html.contact(Content.parse(sheet)))
+            })
+        }
     }
 }
--- app/models/Content.scala
+++ app/models/Content.scala
@@ -6,7 +6,8 @@
 
 package models
 
-import play.api.libs.ws.WS
+import play.api.libs.ws.{WS,Response}
+import play.api.libs.concurrent.Promise
 import play.api.Play
 import play.api.Play.{configuration,current}
 import scala.io._
@@ -14,31 +15,22 @@ import scala.io._
 object Content {
 
     /**
-     * Content of the site
-     *
-     */
-    var content:scala.collection.mutable.Map[String,String] = scala.collection.mutable.Map.empty[String,String]
-
-
-    /**
      * Get a copy of the document
      *
      */
-    def getContent = {
-        WS.url(Play.configuration.getString("application.content").get).get().map { response =>
-            response.body.split("\n").map(line => {
-                val pair = line.split(",")
-                content(pair(0)) = pair(1)
-            })
-        }
-    }
+    def getContent = WS.url(Play.configuration.getString("application.content").get).get()
+
 
     /**
-     * Get a value from a key
+     * Parse sheet
      *
      */
-    def fetch(key:String):Option[String] = {
-        if (content.values.size == 0) getContent
-        content.get(key)
+    def parse(sheet:Response):Map[String,Seq[String]] = {
+        var content:scala.collection.mutable.Map[String,Seq[String]] = scala.collection.mutable.Map.empty[String,Seq[String]]
+        sheet.body.split("\n").map(line => {
+            val pair = line.split(",")
+            content(pair(0)) = pair.tail
+        })
+        content.toMap
     }
 }
