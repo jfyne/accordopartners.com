286b162032e166f3a8879d6cd96b707bcb39acf1
Josh Fyne <josh.fyne@essencedigital.com>
2014-03-03 19:49:05 -0500

    Partners section now has a description

--- app/models/Partners.scala
+++ app/models/Partners.scala
@@ -11,7 +11,7 @@ import play.api.libs.concurrent.Promise
 import play.api.Play
 import play.api.Play.{configuration,current}
 
-case class Partner(name:String, link:String, logo:String)
+case class Partner(name:String, link:String, logo:String, description:String)
 
 object Partner {
 
@@ -32,8 +32,8 @@ object Partner {
         sheet.body.split("\n").map(line => {
             if (lineNum != 0) {
                 val data = line.split("\t")
-                if (data.length == 3) {
-                    partners += Partner(data(0), data(1), data(2))
+                if (data.length == 4) {
+                    partners += Partner(data(0), data(1), data(2), data(3))
                 }
             }
             lineNum = lineNum + 1
